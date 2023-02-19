29ba2635bb91f3dd3e08dbf78fe9909d8c0a1af4
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2023-02-19 13:49:49 +0100

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,9 +7,9 @@ import {SiUpwork} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="httms://linkedin/in/ramziben" target="__blank" title='Linkedin Profile'><BsLinkedin /></a>
-        <a href="httms://github.com/peterondy" target="__blank" title='Github Profile'><AiOutlineGithub /></a>
-        <a href="httms://peterondy.github.io" target="__blank" title='Gitub Static Page'><FaGithubSquare /></a>
+        <a href="https://linkedin/in/ramziben" target="__blank" title='Linkedin Profile'><BsLinkedin /></a>
+        <a href="https://github.com/peterondy" target="__blank" title='Github Profile'><AiOutlineGithub /></a>
+        <a href="https://peterondy.github.io" target="__blank" title='Gitub Static Page'><FaGithubSquare /></a>
         <a href="https://www.upwork.com/freelancers/~018d2cf7cd086a663e" target="__blank" title='Hire Me On Upwork'><SiUpwork /></a>
     </div>
   )
