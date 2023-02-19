46672dff4c1db3b2e46e7e03e93eb1fcbd5cd7c3
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2023-02-19 06:34:16 +0100

    Create CTA.jsx

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CV from '../../assets/cv.pdf'
+import {SiUpwork} from 'react-icons/si'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href="https://www.upwork.com/freelancers/~018d2cf7cd086a663e" className='btn btn-primary'>Hire ME On Upwork<SiUpwork id='upwork'/></a>
+    </div>
+  )
+}
+
+export default CTA
