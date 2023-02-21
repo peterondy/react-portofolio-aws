83dea4a5dbd3ff905f6bf969021111b46925a8cf
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2023-02-21 10:20:55 +0100

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -46,7 +46,7 @@ const Testimonials = () => {
             return(
               <SwiperSlide className="testimonial">
                 <div className="client__avatar">
-                  <img src={avatar} alt="Avatar 1" />
+                  <img src={avata} alt="Avatar 1" />
                 </div>
                 <h5 className="client__name">{name}</h5>
                 <small className='client__review'>{review}</small>
