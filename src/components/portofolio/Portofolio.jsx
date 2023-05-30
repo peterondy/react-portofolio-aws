114087b486100b3191eb5146470bbba2fb517c1a
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2023-05-30 21:15:07 +0100

    Update Portofolio.jsx

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -14,14 +14,14 @@ const data = [
     image: IMG1,
     title: 'React Meteo App',
     github: 'https://github.com/peterondy/meteo-app-react',
-    demo: 'https://github.com/peterondy/meteo-app-react'
+    demo: 'https://645456f2312e51367024f79c--lovely-kangaroo-bb8116.netlify.app/'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'WordPress Development',
-    github: 'https://github.com',
-    demo: 'htpps://github.com'
+    title: 'Paxon Tools',
+    github: 'https://github.com/peterondy/paxontools.github.io',
+    demo: 'https://paxontools.onrender.com/'
   },
   {
     id: 3,
