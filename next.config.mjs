f4447dcca6af59368041e204b924c94f4d972393
Ramzi Ben <90659432+peterondy@users.noreply.github.com>
2024-02-11 04:44:51 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        //pathname: '/account123/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ramzi-ben-official-website.onrender.com',
+        port: ''
+      },
+    ],
+  },
+};
+
+export default nextConfig;
